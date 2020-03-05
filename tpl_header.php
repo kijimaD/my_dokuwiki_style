3bed7829a765358c92f8a5e91d491d2e2dc97a4b
kijima <norimaking777@gmail.com>
2020-03-06 02:45:14 +0900

    検索を追加した

--- tpl_header.php
+++ tpl_header.php
@@ -76,42 +76,54 @@ if (!defined('DOKU_INC')) die();
 						<!-- ページメニュー -->
 						<li class="nav-item mx-2 dropdown">
 							<a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Page</a>
-								<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-									<?php
-									$items = (new \dokuwiki\Menu\PageMenu())->getItems();
-									foreach($items as $item) {
-										echo '<a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
-										   . $item->getLabel()
-										   . '</a>';
-									}
-									?>
-								</div>
-							</li>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<?php
+								$items = (new \dokuwiki\Menu\PageMenu())->getItems();
+								foreach($items as $item) {
+									echo '<a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
+									   . $item->getLabel()
+									   . '</a>';
+								}
+								?>
+							</div>
+						</li>
 
-						<!-- SITE TOOLS -->
+						<!-- 検索 -->
 						<div id="dokuwiki__sitetools">
+							<li class="nav-item mx-2">
+								<?php /*tpl_searchform();*/ ?>
+								<form action="/dokuwiki/doku.php?id=start" method="get" role="search" class="search doku_form" id="dw__search" accept-charset="utf-8"><input type="hidden" name="do" value="search" />
+									<input type="hidden" name="id" value="start" /><div class="no">
+										<input name="q" type="text" class="edit" title="[F]" accesskey="f" placeholder="検索" autocomplete="on" id="qsearch__in" value="" />
+										<button value="1" type="submit" title="検索" class="btn btn-outline-secondary">検索</button><div id="qsearch__out" class="ajax_qsearch JSpopup">
+										</div>
+									</div>
+								</form>
+							</li>
+						</div>
 
-				<?php /*tpl_searchform();*/ ?>
-				<?php /*echo (new \dokuwiki\Menu\MobileMenu())->getDropdown($lang['tools']);*/ ?>
-			</div>
+						<!-- モバイルツール -->
+						<div id="dokuwiki__mobiletools">
+							<?php /*echo (new \dokuwiki\Menu\MobileMenu())->getDropdown($lang['tools']);*/ ?>
+						</div>
 
-			<!-- BREADCRUMBS -->
-			<?php if($conf['breadcrumbs'] || $conf['youarehere']): ?>
-				<!-- <div class="breadcrumbs">
-					 <?php if($conf['youarehere']): ?>
-					 <div class="youarehere"><?php tpl_youarehere() ?></div>
-					 <?php endif ?>
-					 <?php if($conf['breadcrumbs']): ?>
-					 <div class="trace"><?php tpl_breadcrumbs() ?></div>
-					 <?php endif ?>
-					 </div> -->
-			<?php endif ?>
+						<!-- BREADCRUMBS -->
+						<?php if($conf['breadcrumbs'] || $conf['youarehere']): ?>
+							<!-- <div class="breadcrumbs">
+								 <?php if($conf['youarehere']): ?>
+								 <div class="youarehere"><?php tpl_youarehere() ?></div>
+								 <?php endif ?>
+								 <?php if($conf['breadcrumbs']): ?>
+								 <div class="trace"><?php tpl_breadcrumbs() ?></div>
+								 <?php endif ?>
+								 </div> -->
+						<?php endif ?>
 
 
 
-			<hr class="a11y" />
+						<hr class="a11y" />
 		</ul>
 	</div>
-	</div>
-</nav>
+		</div>
+	</nav>
 </div></div><!-- /header -->
