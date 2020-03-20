8a808dcd3fd3e7014d1602bb8669513c6ed2e105
kijima <norimaking777@gmail.com>
2020-03-20 22:47:48 +0900

    Fix navbar-dropdown

--- tpl_header.php
+++ tpl_header.php
@@ -8,44 +8,44 @@ if (!defined('DOKU_INC')) die();
 ?>
 
 <!-- ********** HEADER ********** -->
-    <?php tpl_includeFile('header.html') ?>
+<?php tpl_includeFile('header.html') ?>
 
-	<!-- ナビゲーションバー -->
-	<nav class="navbar navbar-expand-md navbar-light">
+<!-- ナビゲーションバー -->
+<nav class="navbar navbar-expand-md navbar-light">
 
-		<!-- サブコンポーネント -->
-		<div class="container">
+	<!-- サブコンポーネント -->
+	<div class="container">
 
-			<!-- ブランド ..ルートの指定方法がわからないのでphpを使う意味がない状態になっている。-->
-			<?php
-				echo '<a class="navbar-brand" style="font-weight:900;" href="'
-					 .'/doku.php?id=start">'
-                     .$conf['title'].'</a>';
-			?>
+		<!-- ブランドのhref ..ルートの指定方法がわからないのでphpを使う意味がない状態になっている。-->
+		<?php
+		echo '<a class="navbar-brand" style="font-weight:900;" href="'
+			.'/doku.php?id=start">'
+            .$conf['title'].'</a>';
+		?>
 
-				<!-- タグライン？ -->
-				<?php if ($conf['tagline']): ?>
-					<p class="claim"><?php echo $conf['tagline']; ?></p>
-				<?php endif ?>
+		<!-- タグライン？ -->
+		<?php if ($conf['tagline']): ?>
+			<p class="claim"><?php echo $conf['tagline']; ?></p>
+		<?php endif ?>
 
-	<!-- ハンバーガーボタン -->
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+		<!-- ハンバーガーボタン -->
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-    <!-- <ul class="a11y skip">
-         <li><a href="#dokuwiki__content"><?php echo $lang['skip_to_content']; ?></a></li>
-		 </ul> -->
+		<!-- <ul class="a11y skip">
+			 <li><a href="#dokuwiki__content"><?php echo $lang['skip_to_content']; ?></a></li>
+			 </ul> -->
 
-	<!-- ナビゲーション -->
-	<div class="collapse navbar-collapse" id="navbar-content">
+		<!-- ナビゲーション -->
+		<div class="collapse navbar-collapse" id="navbar-content">
 
-		<ul class="navbar-nav mr-auto">
-		</ul>
+			<ul class="navbar-nav mr-auto">
+			</ul>
 
-		<!-- トップメニュー -->
-		<ul class="navbar-nav py-3">
-			<!-- USER TOOLS -->
+			<!-- トップメニュー -->
+			<ul class="navbar-nav py-3">
+				<!-- USER TOOLS -->
 
 				<!-- ドロップダウン(ユーザメニュー) -->
 				<li class="nav-item mx-2 dropdown">
@@ -59,46 +59,47 @@ if (!defined('DOKU_INC')) die();
 							}
 							?>
 					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
 						<?php
 						$items = (new \dokuwiki\Menu\UserMenu())->getItems();
 						foreach($items as $item) {
-							echo '<a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
+							echo '<li><a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
 							   . $item->getLabel()
-							   . '</a>';
+							   . '</a></li>';
 						}
 						?>
-					</div>
+					</ul>
 				</li>
 						<?php endif ?>
 
 						<!-- ページメニュー -->
 						<li class="nav-item mx-2 dropdown">
 							<a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Page</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
 								<?php
 								$items = (new \dokuwiki\Menu\PageMenu())->getItems();
 								foreach($items as $item) {
-									echo '<a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
+									echo '<li><a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
 									   . $item->getLabel()
-									   . '</a>';
+									   . '</a></li>';
 								}
 								?>
-							</div>
+							</ul>
 						</li>
 
 						<!-- モバイルツール -->
-						<li class="nav-item mx-2">
+						<li class="nav-item mx-2 dropdown">
 							<a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Mobile</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
 								<?php
 								$items = (new \dokuwiki\Menu\MobileMenu())->getItems();
 								foreach($items as $item) {
-									echo '<a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
+									echo '<li><a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
 									   . $item->getLabel()
-									   . '</a>';
+									   . '</a></li>';
 								}
 								?>
+							</ul>
 						</li>
 
 						<!-- 検索 -->
@@ -108,8 +109,8 @@ if (!defined('DOKU_INC')) die();
 								<input type="hidden" name="id" value="start" /><div class="no">
 									<input name="q" type="text" class="edit" title="[F]" accesskey="f" placeholder="" autocomplete="on" id="qsearch__in" value="" />
 									<button value="1" type="submit" title="検索" class="btn">検索</button>
-									</div>
 								</div>
+							</div>
 							</form>
 						</li>
 
@@ -128,8 +129,8 @@ if (!defined('DOKU_INC')) die();
 
 
 						<hr class="a11y" />
-		</ul>
-	</div>
+			</ul>
 		</div>
-	</nav>
+	</div>
+</nav>
 <!-- /header -->
